f23b56537626fa2bbb41375936e5a762fde6c1e6
John Buck <johnbrvc@gmail.com>
2023-06-22 11:28:35 -0400

    Added more test scripts Added interactive only script (no sandbox)

--- interactive-problem-gny/pc2_interactive.sh
+++ interactive-problem-gny/pc2_interactive.sh
@@ -1,17 +1,15 @@
 #!/bin/bash
 # Copyright (C) 1989-2023 PC2 Development Team: John Clevenger, Douglas Lane, Samir Ashoo, and Troy Boudreau.
 #  
-# File:    pc2sandbox_interacive.sh 
-# Purpose: a sandbox for pc2 using Linux CGroups v2 and supporting interactive problems
+# File:    pc2_interacive.sh 
+# Purpose: run interactive submission with interative validator
 # Input arguments:
-#  $1: memory limit in MB
-#  $2: time limit in seconds
-#  $3: interactive-validator
-#  $4: judge.in
-#  $5: judge.out
-#  $6: testcase number
-#  $7: command to be executed
-#  $8... : command arguments
+#  $1: interactive-validator
+#  $2: judge.in
+#  $3: judge.out
+#  $4: testcase number
+#  $5: command to be executed
+#  $6... : command arguments
 # 
 # Author: John Buck, based on earlier versions by John Clevenger and Doug Lane
 
@@ -22,15 +20,7 @@ FAIL_RETCODE_BASE=192
 FAIL_EXIT_CODE=$((FAIL_RETCODE_BASE+43))
 FAIL_NO_ARGS_EXIT_CODE=$((FAIL_RETCODE_BASE+44))
 FAIL_INSUFFICIENT_ARGS_EXIT_CODE=$((FAIL_RETCODE_BASE+45))
-FAIL_INVALID_CGROUP_INSTALLATION=$((FAIL_RETCODE_BASE+46))
-FAIL_MISSING_CGROUP_CONTROLLERS_FILE=$((FAIL_RETCODE_BASE+47))
-FAIL_MISSING_CGROUP_SUBTREE_CONTROL_FILE=$((FAIL_RETCODE_BASE+48))
-FAIL_CPU_CONTROLLER_NOT_ENABLED=$((FAIL_RETCODE_BASE+49))
-FAIL_MEMORY_CONTROLLER_NOT_ENABLED=$((FAIL_RETCODE_BASE+50))
-FAIL_MEMORY_LIMIT_EXCEEDED=$((FAIL_RETCODE_BASE+51))
-FAIL_TIME_LIMIT_EXCEEDED=$((FAIL_RETCODE_BASE+52))
 FAIL_WALL_TIME_LIMIT_EXCEEDED=$((FAIL_RETCODE_BASE+53))
-FAIL_SANDBOX_ERROR=$((FAIL_RETCODE_BASE+54))
 FAIL_INTERACTIVE_ERROR=$((FAIL_RETCODE_BASE+55))
 
 # Maximum number of sub-processes before we will kill it due to fork bomb
@@ -43,19 +33,6 @@ CPUMASK=0x08
 # Process ID of submission
 submissionpid=""
 
-CGROUP_PATH=/sys/fs/cgroup
-PC2_CGROUP_PATH=$CGROUP_PATH/pc2
-
-# Name of sandbox for this run
-PC2_SANDBOX_CGROUP_PATH=$PC2_CGROUP_PATH/sandbox_${USER}
-
-# Create unique sandbox name, if possible
-SESSIONID=`ps -ho sess $$`
-# If session is a valid number, tack that on for the unique ID
-if ((SESSIONID))
-then
-	PC2_SANDBOX_CGROUP_PATH="${PC2_SANDBOX_CGROUP_PATH}_$((SESSIONID))"
-fi
 # For interactive validation
 INFIFO=fin
 OUTFIFO=fout
@@ -74,7 +51,7 @@ REPORTDIR=reports
 
 # control whether the script outputs debug/tracing info
 _DEBUG="on"   # change this to anything other than "on" to disable debug/trace output
-DEBUG_FILE=sandbox.log
+DEBUG_FILE=interactive.log
 function DEBUG()
 {
   [ "$_DEBUG" == "on" ] && $@ >> $DEBUG_FILE
@@ -104,10 +81,8 @@ function REPORT_DEBUG()
 usage()
 {
   cat <<SAGE
-Usage: pc2sandbox_interactive.sh memlimit timelimit interactive-validator judgein judgeans testcase command command_args
+Usage: pc2_interactive.sh interactive-validator judgein judgeans testcase command command_args
 
-memlimit, in MB
-timelimit, in seconds
 interactive-validator, judge's program to execute to provide interactive responses to the submission
 judgein, judges input file
 judgeans, judges answer file
@@ -127,7 +102,7 @@ KillValidator()
 }
 
 # Function to handle getting killed by PC2's execute timer (basically, this
-# is wall-time exceeded which is execute time limit + 1 second
+# is wall-time exceeded which is execute time limit limit
 HandleTerminateFromPC2()
 {
 	DEBUG echo "Received TERMINATE signal from PC2"
@@ -178,68 +153,20 @@ if [ "$1" = "-h" -o "$1" = "--help" ] ; then
    exit $FAIL_EXIT_CODE
 fi 
 
-if [ "$#" -lt 7 ] ; then
-   echo $0: expected 7 or more arguments, found: $*
+if [ "$#" -lt 5 ] ; then
+   echo $0: expected 5 or more arguments, found: $*
    exit $FAIL_INSUFFICIENT_ARGS_EXIT_CODE
 fi 
 
-MEMLIMIT=$1
-TIMELIMIT=$2
-VALIDATOR="$3"
-JUDGEIN="$4"
-JUDGEANS="$5"
-TESTCASE="$6"
-COMMAND="$7"
-shift 7
+VALIDATOR="$1"
+JUDGEIN="$2"
+JUDGEANS="$3"
+TESTCASE="$4"
+COMMAND="$5"
+shift 5
 
 # the rest of the commmand line arguments  are the command args for the submission
 
-
-# make sure we have CGroups V2 properly installed on this system, including a PC2 structure
-
-DEBUG echo checking PC2 CGroup V2 installation...
-if [ ! -d "$PC2_CGROUP_PATH" ]; then
-   echo $0: expected pc2sandbox CGroups v2 installation in $PC2_CGROUP_PATH 
-   exit $FAIL_INVALID_CGROUP_INSTALLATION
-fi
-
-if [ ! -f "$CGROUP_PATH/cgroup.controllers" ]; then
-   echo $0: missing file cgroup.controllers in $CGROUP_PATH
-   exit $FAIL_MISSING_CGROUP_CONTROLLERS_FILE
-fi
-
-if [ ! -f "$CGROUP_PATH/cgroup.subtree_control" ]; then
-   echo $0: missing file cgroup.subtree_control in $CGROUP_PATH
-   exit $FAIL_MISSING_CGROUP_SUBTREE_CONTROL_FILE
-fi
-
-# make sure the cpu and memory controllers are enabled
-if ! grep -q -F "cpu" "$CGROUP_PATH/cgroup.subtree_control"; then
-   echo $0: cgroup.subtree_control in $CGROUP_PATH does not enable cpu controller
-   exit $FAIL_CPU_CONTROLLER_NOT_ENABLED
-fi
-
-if ! grep -q -F "memory" "$CGROUP_PATH/cgroup.subtree_control"; then
-   echo $0: cgroup.subtree_control in $CGROUP_PATH does not enable memory controller
-   exit $FAIL_MEMORY_CONTROLLER_NOT_ENABLED
-fi
-
-
-# we seem to have a valid CGroup installation
-DEBUG echo ...done.
-
-if test -d $PC2_SANDBOX_CGROUP_PATH
-then
-	DEBUG echo Removing existing sandbox to start clean
-	rmdir $PC2_SANDBOX_CGROUP_PATH
-fi
-
-DEBUG echo Creating sandbox $PC2_SANDBOX_CGROUP_PATH
-if ! mkdir $PC2_SANDBOX_CGROUP_PATH
-then
-	DEBUG echo Cannot create $PC2_SANDBOX_CGROUP_PATH
-	exit $FAIL_INVALID_CGROUP_INSTALLATION
-fi
 
 # First, make the fifos for the pipework between the submission and interactive validator
 rm -f "$INFIFO" "$OUTFIFO"
@@ -261,20 +188,6 @@ mkdir -p "$REPORTDIR"
 # Set report file to be testcase specific one now
 REPORTFILE=`printf "$REPORTDIR/testcase_%03d.log" $TESTCASE`
 
-# set the specified memory limit - input is in MB, cgroup v2 requires bytes, so multiply by 1M
-# but only if > 0.
-# "max" means unlimited, which is the cgroup v2 default
-DEBUG echo checking memory limit
-if [ "$MEMLIMIT" -gt "0" ] ; then
-  REPORT Setting memory limit to $MEMLIMIT MB
-  echo $(( $MEMLIMIT * 1024 * 1024 ))  > $PC2_SANDBOX_CGROUP_PATH/memory.max
-  echo 1  > $PC2_SANDBOX_CGROUP_PATH/memory.swap.max
-else
-  REPORT Setting memory limit to max, meaning no limit
-  echo "max" > $PC2_SANDBOX_CGROUP_PATH/memory.max  
-  echo "max" > $PC2_SANDBOX_CGROUP_PATH/memory.swap.max  
-fi
-
 # Set up trap handler to catch wall-clock time exceeded and getting killed by PC2's execute timer
 trap HandleTerminateFromPC2 2 15
 
@@ -290,27 +203,8 @@ $VALIDATOR $JUDGEIN $JUDGEANS $INT_FEEDBACKDIR > $INFIFO < $OUTFIFO &
 intv_pid=$!
 REPORT Started interactive validator PID $intv_pid
 
-# We use ulimit to limit CPU time, not cgroups.  Time is supplied in seconds.  This may have to
-# be reworked if ms accuracy is needed.  The problem is, cgroups do not kill off a process that
-# exceeds the time limit, ulimit does.
-TIMELIMIT_US=$((TIMELIMIT * 1000000))
-REPORT Setting cpu limit to $TIMELIMIT_US microseconds "("ulimit -t $TIMELIMIT ")"
-ulimit -t $TIMELIMIT
-
-REPORT Setting maximum user processes to $MAXPROCS + whatever the user is currently using
-ulimit -u $((MAXPROCS+`ps -T -u $USER | wc -l`))
-
 # Remember wall time when we started
 starttime=`GetTimeInMicros`
-
-#put the current process (and implicitly its children) into the pc2sandbox cgroup.
-REPORT Putting $$ into $PC2_SANDBOX_CGROUP_PATH cgroup
-if ! echo $$ > $PC2_SANDBOX_CGROUP_PATH/cgroup.procs
-then
-	echo $0: Could not add current process to $PC2_SANDBOX_CGROUP_PATH/cgroup.procs - not executing submission.
-	rm -f "$INFIFO" "$OUTFIFO"
-	exit $FAIL_SANDBOX_ERROR
-fi
 
 # run the command
 REPORT_DEBUG Executing "taskset $CPUMASK $COMMAND $* < $INFIFO > $OUTFIFO"
@@ -375,41 +269,19 @@ do
 	# If this is the contestant submission
 	if test "$child_pid" -eq "$submissionpid"
 	then
-		# Get cpu time immediately to minimize any usage by this shell
-		cputime=`grep usage_usec $PC2_SANDBOX_CGROUP_PATH/cpu.stat | cut -d ' ' -f 2`
-		# Get wall time - we want it as close as possible to when we fetch the cpu time so they stay close
-		# since the cpu.stat includes the time this script takes AFTER the submission finishes.
+		# Get wall time
 		endtime=`GetTimeInMicros`
 
 		REPORT Contestant PID $submissionpid finished status $wstat
 		contestant_done=1
 		COMMAND_EXIT_CODE=$wstat
 
-		# See if we were killed due to memory - this is a kill 9 if it happened
-		kills=`grep oom_kill $PC2_SANDBOX_CGROUP_PATH/memory.events | cut -d ' ' -f 2`
-		
-		if test "$kills" != "0"
+		if test "$COMMAND_EXIT_CODE" -ge 128
 		then
-			REPORT_DEBUG The command was killed due to out of memory
-			COMMAND_EXIT_CODE=${FAIL_MEMORY_LIMIT_EXCEEDED}
-			GenXML "No - Memory limit exceeded" ""
-			KillValidator
-			break
+			REPORT_DEBUG The command terminated abnormally due to a signal with exit code $COMMAND_EXIT_CODE
 		else
-			if test "$cputime" -gt "$TIMELIMIT_US"
-			then
-				REPORT_DEBUG The command was killed because it exceeded the CPU Time limit "(${cputime}us > ${TIMELIMIT_US}us)"
-				COMMAND_EXIT_CODE=${FAIL_TIME_LIMIT_EXCEEDED}
-				GenXML "No - Time limit exceeded" "${cputime}us > ${TIMELIMIT_US}us"
-				KillValidator
-				break
-			elif test "$COMMAND_EXIT_CODE" -ge 128
-			then
-				REPORT_DEBUG The command terminated abnormally due to a signal with exit code $COMMAND_EXIT_CODE
-			else
-				walltime=$((endtime-starttime))
-				REPORT_DEBUG The command terminated normally and took ${cputime}us of CPU time "(out of the CPU limit of ${TIMELIMIT_US}us)" and ${walltime}us wall time
-			fi
+			walltime=$((endtime-starttime))
+			REPORT_DEBUG The command terminated normally and took ${walltime}us wall time
 		fi
 		REPORT_DEBUG Finished executing "$COMMAND $*"
 		REPORT_DEBUG "$COMMAND" exited with exit code $COMMAND_EXIT_CODE
@@ -435,5 +307,5 @@ rm -f "$INFIFO" "$OUTFIFO"
 # return the exit code of the command as our exit code
 exit $COMMAND_EXIT_CODE
 
-# eof pc2sandbox_interactive.sh 
+# eof pc2_interactive.sh 
 
